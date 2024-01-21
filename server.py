159e9b33b313f6fe7ec7b30be74fa80587d7d85c
pablo <pabloluceroschneider@gmail.com>
2024-01-21 02:16:37 -0300

    add create user method

--- server.py
+++ server.py
@@ -45,7 +45,16 @@ class BlogPost(db.Model):
 # routes
 @app.route("/users", methods=["POST"])
 def create_user():
-    return jsonify({"message" : "create_user"})
+    data = request.get_json()
+    new_user = User(
+        name=data["name"],
+        email=data["email"],
+        address=data["address"],
+        phone=data["phone"],
+    )
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"messager": "user created"}), 200
 
 @app.route("/users/<user_id>", methods=["GET"])
 def get_user_by_id():
