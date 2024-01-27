0938b5fc588c50f247ea5d2a167cc6dc97c441d7
pablo <pabloluceroschneider@gmail.com>
2024-01-27 13:35:23 -0300

    Add methods

--- src/users/controllers.py
+++ src/users/controllers.py
@@ -14,7 +14,8 @@ def list_users():
     
 def user_by_id(user_id):
     try:
-        return jsonify({ "id": user_id}), 200
+        user = User.query.filter_by(id=user_id).first()
+        return jsonify(user.toDict()), 200
     except Exception as e:
         return jsonify({ "error": e }), 500
     
@@ -28,4 +29,22 @@ def create_user():
     )
     db.session.add(new_user)
     db.session.commit()
-    return jsonify({"messager": "user created"}), 200
+    return jsonify(new_user.toDict()), 201
+
+def update_user(user_id):
+    data = request.get_json()
+    user = User.query.filter_by(id=user_id).first()
+    
+    user.name = data["name"] 
+    user.email = data["email"]
+    user.address = data["address"]
+    user.phone = data["phone"]
+
+    db.session.commit()
+    return jsonify(user.toDict()), 200
+
+def remove_user_by_id(user_id):
+    user = User.query.filter_by(id=user_id).first()
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({ "message": f"User {user.id} has been removed"}), 200
--- src/users/routes.py
+++ src/users/routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 
-from .controllers import list_users, user_by_id, create_user
+from .controllers import list_users, user_by_id, create_user, update_user, remove_user_by_id
 
 users = Blueprint('users', __name__)
 
@@ -17,3 +17,11 @@ def get_list_users():
 @users.route(f'{base_url}/<user_id>', methods=["GET"])
 def get_user_by_id(user_id):
     return user_by_id(user_id)
+
+@users.route(f'{base_url}/<user_id>', methods=["PUT"])
+def put_update_user(user_id):
+    return update_user(user_id)
+
+@users.route(f'{base_url}/<user_id>', methods=["DELETE"])
+def delete_remove_user_by_id(user_id):
+    return remove_user_by_id(user_id)
