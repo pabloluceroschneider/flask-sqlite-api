e04270d390d720183899485d2a601386428925b9
pablo <pabloluceroschneider@gmail.com>
2024-01-27 15:59:40 -0300

    Add auth/login route

---
+++ src/auth/services.py
@@ -0,0 +1,20 @@
+import os
+import jwt, datetime
+
+secret_key = os.getenv("APP_SECRET_KEY")
+
+class AuthServices:
+    def login(body):
+        payload = {
+            "user": body["user"],
+            "password": body["password"],
+            "exp": datetime.datetime.now() + datetime.timedelta(hours=1),
+            "iat": datetime.datetime.now()
+        }
+
+        try:
+            jwt_token = jwt.encode(payload, secret_key, algorithm="HS256")
+        except Exception as e:
+            print(e)
+            return f"Error: {e}"
+        return jwt_token
--- src/routes.py
+++ src/routes.py
@@ -1,7 +1,7 @@
-from .auth import auth
+from .auth import routes as authRoutes
 from .users import routes as userRoutes
 
 def routes(app):
-    app.register_blueprint(auth)
+    app.register_blueprint(authRoutes.auth)
     app.register_blueprint(userRoutes.users)
     return app
