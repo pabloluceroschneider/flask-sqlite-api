1ee1a163c5442874efd38ab88a9b73a36782060d
Pablo Lucero Schneider <43233080+pabloluceroschneider@users.noreply.github.com>
2024-01-27 16:57:12 -0300

    Feature/Add try-catch (#11)

--- src/users/controllers.py
+++ src/users/controllers.py
@@ -21,31 +21,40 @@ class UserController:
             return jsonify({ "error": e }), 500
 
     def create_user():
-        data = request.get_json()
-        new_user = User(
-            name=data["name"],
-            email=data["email"],
-            address=data["address"],
-            phone=data["phone"],
-        )
-        db.session.add(new_user)
-        db.session.commit()
-        return jsonify(new_user.toDict()), 201
+        try:
+            data = request.get_json()
+            new_user = User(
+                name=data["name"],
+                email=data["email"],
+                address=data["address"],
+                phone=data["phone"],
+            )
+            db.session.add(new_user)
+            db.session.commit()
+            return jsonify(new_user.toDict()), 201
+        except Exception as e:
+            return jsonify({ "error": e }), 500
 
     def update_user(user_id):
-        data = request.get_json()
-        user = User.query.filter_by(id=user_id).first()
+        try:
+            data = request.get_json()
+            user = User.query.filter_by(id=user_id).first()
 
-        user.name = data["name"] 
-        user.email = data["email"]
-        user.address = data["address"]
-        user.phone = data["phone"]
+            user.name = data["name"] 
+            user.email = data["email"]
+            user.address = data["address"]
+            user.phone = data["phone"]
 
-        db.session.commit()
-        return jsonify(user.toDict()), 200
+            db.session.commit()
+            return jsonify(user.toDict()), 200
+        except Exception as e:
+            return jsonify({ "error": e }), 500
 
     def remove_user_by_id(user_id):
-        user = User.query.filter_by(id=user_id).first()
-        db.session.delete(user)
-        db.session.commit()
-        return jsonify({ "message": f"User with id '{user.id}' has been successfully removed"}), 200
+        try:
+            user = User.query.filter_by(id=user_id).first()
+            db.session.delete(user)
+            db.session.commit()
+            return jsonify({ "message": f"User with id '{user.id}' has been successfully removed"}), 200
+        except Exception as e:
+            return jsonify({ "error": e }), 500
