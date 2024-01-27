5ca07ea2b95a4d8ce54034a476e321567d013aff
Pablo Lucero Schneider <43233080+pabloluceroschneider@users.noreply.github.com>
2024-01-27 16:42:08 -0300

    Improvements (#10)

--- src/users/routes.py
+++ src/users/routes.py
@@ -1,27 +1,27 @@
 from flask import Blueprint
 
-from .controllers import list_users, user_by_id, create_user, update_user, remove_user_by_id
+from .controllers import UserController
 
 users = Blueprint('users', __name__)
 
 base_url = '/users'
 
 @users.route(f'{base_url}', methods=["POST"])
-def post_create_user():
-    return create_user()
+def create_user():
+    return UserController.create_user()
 
 @users.route(f'{base_url}', methods=["GET"])
-def get_list_users():
-    return list_users()
+def list_users():
+    return UserController.list_users()
 
 @users.route(f'{base_url}/<user_id>', methods=["GET"])
-def get_user_by_id(user_id):
-    return user_by_id(user_id)
+def user_by_id(user_id):
+    return UserController.user_by_id(user_id)
 
 @users.route(f'{base_url}/<user_id>', methods=["PUT"])
-def put_update_user(user_id):
-    return update_user(user_id)
+def update_user(user_id):
+    return UserController.update_user(user_id)
 
 @users.route(f'{base_url}/<user_id>', methods=["DELETE"])
-def delete_remove_user_by_id(user_id):
-    return remove_user_by_id(user_id)
+def remove_user_by_id(user_id):
+    return UserController.remove_user_by_id(user_id)
